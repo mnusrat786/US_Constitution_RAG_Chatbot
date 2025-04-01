31c14b0dc70fdda448c9a7b8964f9ca97f6531ed
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2025-04-02 00:03:53 +0500

    Update embedder.py

--- embedder.py
+++ embedder.py
@@ -15,7 +15,6 @@ def create_vector_store(chunks):
 
 
 
-
 # from langchain.vectorstores import FAISS
 # from langchain.embeddings import GoogleGenerativeAIEmbeddings
 # from langchain_community.embeddings import GoogleGenerativeAIEmbeddings
